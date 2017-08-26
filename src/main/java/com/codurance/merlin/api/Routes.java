080503fcd5799412abedad0df8a71a40fa40cdaa
Luciano Palma <lucianopalma90@gmail.com>
2017-08-27 00:04:03 +0100

    load data from JSON

--- src/main/java/com/codurance/merlin/api/Routes.java
+++ src/main/java/com/codurance/merlin/api/Routes.java
@@ -1,16 +1,30 @@
 package com.codurance.merlin.api;
 
+import com.codurance.merlin.Commitment;
 import com.codurance.merlin.infrastructure.AuthenticationFilter;
 import com.codurance.merlin.infrastructure.User;
 import com.codurance.merlin.infrastructure.Authenticator;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-import java.util.HashMap;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Routes {
+
+    public static final String PROJECT_COMMITMENTS = "PROJECT_COMMITMENTS";
+    public static final String PROJECT_COMMITMENTS_FILE = "project_commitments.json";
+
     public void init(Authenticator authenticator, MustacheTemplateEngine templateEngine, AuthenticationFilter filter) {
         port(8080);
 
@@ -29,6 +43,22 @@ public class Routes {
             return null;
         }));
 
-        get("/", (req, res) -> new ModelAndView(new HashMap<>(), "index.mustache"), templateEngine);
+        get("/", (req, res) -> new ModelAndView(loadProjectCommitments(), "index.mustache"), templateEngine);
+    }
+
+    private Map<String, Collection<Commitment>> loadProjectCommitments(){
+        Gson gson = new Gson();
+
+        Type commitmentsMap = new TypeToken<Map<String, Collection<Commitment>>>(){}.getType();
+
+        String commitmentsJson;
+
+        try {
+            commitmentsJson = String.join("", Files.readAllLines(Paths.get(PROJECT_COMMITMENTS_FILE)));
+        } catch (IOException e) {
+            commitmentsJson = System.getenv(PROJECT_COMMITMENTS);
+        }
+
+        return gson.fromJson(commitmentsJson, commitmentsMap);
     }
 }
