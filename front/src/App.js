a2735ac4c01b274b7ded17087f1223c74c7718ec
Luciano Palma <lucianopalma90@gmail.com>
2017-09-03 21:42:45 +0100

    update dragsnap to day interval

--- front/src/App.js
+++ front/src/App.js
@@ -95,6 +95,7 @@ class App extends Component {
                     defaultTimeEnd={moment().add(6, 'month')}
                     //timeSteps={{second: 0, minute: 0, hour: 0, day: 1, month: 1, year: 1}}
                     onItemResize={this.handleCommitmentResize}
+                    dragSnap={60 * 60 * 1000}
                 />
             </div>
        );
