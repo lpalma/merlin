3a92f42ceb8fa8aceddbebae85edb9e9609c2526
Luciano Palma <lucianopalma90@gmail.com>
2017-09-07 15:27:13 +0100

    rename OAuthCondig.build() to .load()

--- src/main/java/com/codurance/merlin/infrastructure/OAuthConfig.java
+++ src/main/java/com/codurance/merlin/infrastructure/OAuthConfig.java
@@ -25,7 +25,7 @@ class OAuthConfig {
         return callbackUrl;
     }
 
-    public OAuthConfig build() {
+    public OAuthConfig load() {
         Properties properties = new Properties();
 
         try {
@@ -39,7 +39,6 @@ class OAuthConfig {
             clientSecret = System.getenv(CLIENT_SECRET);
             callbackUrl = System.getenv(CALLBACK_URL);
         }
-
 
         return this;
     }
