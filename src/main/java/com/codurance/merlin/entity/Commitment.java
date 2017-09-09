0eb84e3bb763a2b53c36e65a43cb81ffdee976d7
Luciano Palma <lucianopalma90@gmail.com>
2017-09-09 21:10:00 +0100

    add ID to entities

--- src/main/java/com/codurance/merlin/entity/Commitment.java
+++ src/main/java/com/codurance/merlin/entity/Commitment.java
@@ -1,18 +1,26 @@
 package com.codurance.merlin.entity;
 
+import com.codurance.merlin.valueObjects.Id;
+
 import java.time.LocalDate;
 
 public class Commitment {
+    private Id id;
     private Employee employee;
     private Project project;
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public Commitment(Employee employee, Project project, LocalDate startDate, LocalDate endDate) {
+    public Commitment(Id id, Employee employee, Project project, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
         this.employee = employee;
         this.project = project;
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    public Id id() {
+        return id;
     }
 
     public Employee employee() {
--- src/main/java/com/codurance/merlin/entity/Project.java
+++ src/main/java/com/codurance/merlin/entity/Project.java
@@ -1,13 +1,21 @@
 package com.codurance.merlin.entity;
 
+import com.codurance.merlin.valueObjects.Id;
+
 public class Project {
+    private Id id;
     private String name;
 
-    public Project(String name) {
+    public Project(Id id, String name) {
+        this.id = id;
         this.name = name;
     }
 
     public String name() {
         return name;
+    }
+
+    public Id id() {
+        return id;
     }
 }
