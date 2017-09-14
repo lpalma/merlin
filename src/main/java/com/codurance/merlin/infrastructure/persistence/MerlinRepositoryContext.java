5dcf8b30402048b8fed2b494cad2c08c266ea708
Luciano Palma <lucianopalma90@gmail.com>
2017-09-14 16:35:23 +0100

    load DB config from ContextClass

--- src/main/java/com/codurance/merlin/infrastructure/persistence/MerlinRepositoryContext.java
+++ src/main/java/com/codurance/merlin/infrastructure/persistence/MerlinRepositoryContext.java
@@ -1,15 +1,32 @@
 package com.codurance.merlin.infrastructure.persistence;
 
 import com.codurance.lightaccess.LightAccess;
-import org.postgresql.jdbc2.optional.PoolingDataSource;
+import com.codurance.merlin.infrastructure.MerlinEnvConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.postgresql.ds.PGSimpleDataSource;
 
 public class MerlinRepositoryContext {
-    public static PostgreSQLCommitmentRepository getCommitmentRepository() {
-        PoolingDataSource ds = new PoolingDataSource();
-        ds.setUser("merlin");
-        ds.setPassword("merlin@local!");
-        ds.setDatabaseName("merlin_development");
 
-        return new PostgreSQLCommitmentRepository(new LightAccess(ds));
+    public static final String USER = "user";
+    public static final String PASSWORD = "password";
+    public static final String DATABASE_NAME = "databaseName";
+    public static final String DATA_SOURCE_CLASS_NAME = PGSimpleDataSource.class.getName();
+
+    public static PostgreSQLCommitmentRepository getCommitmentRepository() {
+        LightAccess lightAccess = new LightAccess(buildDataSource());
+
+        return new PostgreSQLCommitmentRepository(lightAccess);
+    }
+
+    private static HikariDataSource buildDataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+
+        hikariConfig.addDataSourceProperty(USER, MerlinEnvConfig.getDatabaseUser());
+        hikariConfig.addDataSourceProperty(PASSWORD, MerlinEnvConfig.getDatabasePassword());
+        hikariConfig.addDataSourceProperty(DATABASE_NAME, MerlinEnvConfig.getDatabaseName());
+        hikariConfig.setDataSourceClassName(DATA_SOURCE_CLASS_NAME);
+
+        return new HikariDataSource(hikariConfig);
     }
 }
