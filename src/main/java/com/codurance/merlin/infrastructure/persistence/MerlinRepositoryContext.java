8616502edbb857e30e36bfa1463e043890bda3e7
Luciano Palma <lucianopalma90@gmail.com>
2017-09-18 00:48:36 +0100

    refactor Hikari DataSource setup

--- src/main/java/com/codurance/merlin/infrastructure/persistence/MerlinRepositoryContext.java
+++ src/main/java/com/codurance/merlin/infrastructure/persistence/MerlinRepositoryContext.java
@@ -2,14 +2,10 @@ package com.codurance.merlin.infrastructure.persistence;
 
 import com.codurance.lightaccess.LightAccess;
 import com.codurance.merlin.infrastructure.MerlinEnvConfig;
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class MerlinRepositoryContext {
 
-    public static final String USER = "user";
-    public static final String PASSWORD = "password";
-    public static final String DATABASE_NAME = "databaseName";
     public static final String CONNECTION_TEST_QUERY = "SELECT 1";
 
     public static PostgreSQLCommitmentRepository getCommitmentRepository() {
@@ -19,15 +15,14 @@ public class MerlinRepositoryContext {
     }
 
     private static HikariDataSource buildDataSource() {
-        HikariConfig hikariConfig = new HikariConfig();
+        HikariDataSource ds = new HikariDataSource();
 
-        hikariConfig.addDataSourceProperty(USER, MerlinEnvConfig.getDatabaseUser());
-        hikariConfig.addDataSourceProperty(PASSWORD, MerlinEnvConfig.getDatabasePassword());
-        hikariConfig.addDataSourceProperty(DATABASE_NAME, MerlinEnvConfig.getDatabaseName());
-        hikariConfig.setJdbcUrl(MerlinEnvConfig.getDatabaseURL());
-        hikariConfig.setSchema(MerlinEnvConfig.getDatabaseSchema());
-        hikariConfig.setConnectionTestQuery(CONNECTION_TEST_QUERY);
+        ds.setJdbcUrl(MerlinEnvConfig.getDatabaseURL());
+        ds.setUsername(MerlinEnvConfig.getDatabaseUser());
+        ds.setPassword(MerlinEnvConfig.getDatabasePassword());
+        ds.setSchema(MerlinEnvConfig.getDatabaseSchema());
+        ds.setConnectionTestQuery(CONNECTION_TEST_QUERY);
 
-        return new HikariDataSource(hikariConfig);
+        return ds;
     }
 }
