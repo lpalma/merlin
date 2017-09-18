a75d7e624aabc4e6e4fa40cb2e809e5179c165c3
Luciano Palma <lucianopalma90@gmail.com>
2017-09-18 10:18:16 +0100

    remove connection pooling

--- src/main/java/com/codurance/merlin/infrastructure/MerlinEnvConfig.java
+++ src/main/java/com/codurance/merlin/infrastructure/MerlinEnvConfig.java
@@ -7,6 +7,8 @@ public class MerlinEnvConfig {
     public static final String DEFAULT_DATABASE_NAME = "merlin";
     private static final String DATABASE_URL = "DATABASE_URL";
     public static final String DEFAULT_DATABASE_SCHEMA = "merlin";
+    public static final String DATABASE_PORT = "DATABASE_PORT";
+    public static final String DATABASE_HOST = "DATABASE_HOST";
 
     private static final class MerlinEnvConfigHolder {
         private static final MerlinEnvConfig INSTANCE = new MerlinEnvConfig();
@@ -44,5 +46,13 @@ public class MerlinEnvConfig {
 
     public static String getDatabaseSchema() {
         return DEFAULT_DATABASE_SCHEMA;
+    }
+
+    public static String getDatabaseHost() {
+        return instance().get(DATABASE_HOST);
+    }
+
+    public static int getDatabasePortNumber() {
+        return Integer.valueOf(instance().get(DATABASE_PORT));
     }
 }
--- src/main/java/com/codurance/merlin/infrastructure/persistence/MerlinRepositoryContext.java
+++ src/main/java/com/codurance/merlin/infrastructure/persistence/MerlinRepositoryContext.java
@@ -2,11 +2,9 @@ package com.codurance.merlin.infrastructure.persistence;
 
 import com.codurance.lightaccess.LightAccess;
 import com.codurance.merlin.infrastructure.MerlinEnvConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import org.postgresql.ds.PGSimpleDataSource;
 
 public class MerlinRepositoryContext {
-
-    public static final String CONNECTION_TEST_QUERY = "SELECT 1";
 
     public static PostgreSQLCommitmentRepository getCommitmentRepository() {
         LightAccess lightAccess = new LightAccess(buildDataSource());
@@ -14,14 +12,14 @@ public class MerlinRepositoryContext {
         return new PostgreSQLCommitmentRepository(lightAccess);
     }
 
-    private static HikariDataSource buildDataSource() {
-        HikariDataSource ds = new HikariDataSource();
+    private static PGSimpleDataSource buildDataSource() {
+        PGSimpleDataSource ds = new PGSimpleDataSource();
 
-        ds.setJdbcUrl(MerlinEnvConfig.getDatabaseURL());
-        ds.setUsername(MerlinEnvConfig.getDatabaseUser());
+        ds.setDatabaseName(MerlinEnvConfig.getDatabaseName());
+        ds.setServerName(MerlinEnvConfig.getDatabaseHost());
+        ds.setPortNumber(MerlinEnvConfig.getDatabasePortNumber());
+        ds.setUser(MerlinEnvConfig.getDatabaseUser());
         ds.setPassword(MerlinEnvConfig.getDatabasePassword());
-        ds.setSchema(MerlinEnvConfig.getDatabaseSchema());
-        ds.setConnectionTestQuery(CONNECTION_TEST_QUERY);
 
         return ds;
     }
