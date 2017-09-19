d295ff8c9117132292cd5ac32f03568d480c863a
Luciano Palma <lucianopalma90@gmail.com>
2017-09-19 14:01:51 +0100

    reinitialise commitments empty

--- src/main/resources/js/App.js
+++ src/main/resources/js/App.js
@@ -31,58 +31,6 @@ class App extends Component {
     }
 
     componentWillMount() {
-        this.route
-            .getCommitments()
-            .then(commitments => {
-                this.setState((prevState, props) => ({
-                    craftspersons: this.setEmployess(commitments),
-                    commitments: this.setCommitments(commitments)
-                }))
-        })
-    }
-
-    setEmployess(commitments) {
-        let craftspersons = []
-
-        commitments.forEach(commitment => {
-            const id = commitment.craftsperson.id.value
-            const name = commitment.craftsperson.name
-
-            if (!craftspersons.some(e => (e.id == id))) {
-                craftspersons.push({
-                    id: id,
-                    title: name,
-                })
-            }
-        })
-
-        return craftspersons
-    }
-
-    setCommitments(commitments) {
-        let classNames = []
-        let result = []
-
-        commitments.forEach(commitment => {
-            const project = commitment.project
-            const craftsperson = commitment.craftsperson
-            const className = this.calculateClassName(classNames, project.name.toLowerCase())
-
-            if (!result.some(r => (r.group == craftsperson.id.value))) {
-                result.push({
-                    id: commitment.id.value,
-                    group: craftsperson.id.value,
-                    title: project.name,
-                    start_time: this.formatDate(commitment.startDate),
-                    end_time: this.formatDate(commitment.endDate),
-                    canMove: false,
-                    canResize: 'both',
-                    className: className,
-                })
-            }
-        })
-
-        return result
     }
 
     formatDate = (date) => {
@@ -119,8 +67,8 @@ class App extends Component {
             <div>
                 <Header />
                 <div className="container-fluid commitments-board">
-                    <Timeline groups={this.state.craftspersons}
-                        items={this.state.commitments}
+                    <Timeline groups={[]}
+                        items={[]}
                         defaultTimeStart={moment()}
                         defaultTimeEnd={moment().add(6, 'month')}
                         timeSteps={{second: 0, minute: 0, hour: 0, day: 1, month: 1, year: 1}}
