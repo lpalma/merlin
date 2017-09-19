b943899a5a13650148360728170bae00cfaaec71
Luciano Palma <lucianopalma90@gmail.com>
2017-09-19 14:18:53 +0100

    create craftspeople API with some dummy data

--- src/main/resources/js/Routes.js
+++ src/main/resources/js/Routes.js
@@ -1,11 +1,36 @@
 const axios = require('axios')
+const craftspeople = [
+    {
+        "id": "rollo@codurance.com",
+        "name": "Rollo Lothbrok",
+        "email": "rollo@codurance.com",
+        "role": "admin",
+        "active": true
+    },
+    {
+        "id": "lagertha@codurance.com",
+        "name": "Lagertha",
+        "email": "lagertha@codurance.com",
+        "role": "admin",
+        "active": true
+    },
+    {
+        "id": "floki@codurance.com",
+        "name": "Floki",
+        "email": "floki@codurance.com",
+        "role": "user",
+        "active": true
+    }]
 
 class Route {
-
     getCommitments() {
         return axios.get('/api/commitments').then((response) => {
             return Promise.resolve(response.data)
         })
+    }
+
+    allCraftspeople() {
+        return Promise.resolve(craftspeople)
     }
 }
 
