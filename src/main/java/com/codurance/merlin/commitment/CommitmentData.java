18666cbd115bf43a134e8184d055e4151c884be5
Luciano Palma <lucianopalma90@gmail.com>
2017-09-25 17:20:49 +0100

    create CommitmentData

---
+++ src/main/java/com/codurance/merlin/commitment/CommitmentData.java
@@ -0,0 +1,55 @@
+package com.codurance.merlin.commitment;
+
+import java.time.LocalDate;
+
+public class CommitmentData {
+    private CraftspersonId craftspersonId;
+    private ProjectId projectId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public CommitmentData(CraftspersonId craftspersonId, ProjectId projectId, LocalDate startDate, LocalDate endDate) {
+        this.craftspersonId = craftspersonId;
+        this.projectId = projectId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public CraftspersonId craftspersonId() {
+        return craftspersonId;
+    }
+
+    public ProjectId projectId() {
+        return projectId;
+    }
+
+    public LocalDate startDate() {
+        return startDate;
+    }
+
+    public LocalDate endDate() {
+        return endDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CommitmentData that = (CommitmentData) o;
+
+        if (!craftspersonId.equals(that.craftspersonId)) return false;
+        if (!projectId.equals(that.projectId)) return false;
+        if (!startDate.equals(that.startDate)) return false;
+        return endDate.equals(that.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = craftspersonId.hashCode();
+        result = 31 * result + projectId.hashCode();
+        result = 31 * result + startDate.hashCode();
+        result = 31 * result + endDate.hashCode();
+        return result;
+    }
+}
