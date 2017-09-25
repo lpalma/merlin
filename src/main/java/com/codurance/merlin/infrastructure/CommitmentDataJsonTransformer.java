72e3b5eae6994be1ddf8d137289e406c0f611da0
Luciano Palma <lucianopalma90@gmail.com>
2017-09-25 17:22:38 +0100

    create json transformer for CommitmentData

---
+++ src/main/java/com/codurance/merlin/infrastructure/CommitmentDataJsonTransformer.java
@@ -0,0 +1,30 @@
+package com.codurance.merlin.infrastructure;
+
+import com.codurance.merlin.commitment.CommitmentData;
+import com.codurance.merlin.commitment.CraftspersonId;
+import com.codurance.merlin.commitment.ProjectId;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.time.LocalDate;
+
+public class CommitmentDataJsonTransformer {
+
+    public static final String CRAFTSPERSON_ID = "craftspersonId";
+    public static final String PROJECT_ID = "projectId";
+    public static final String START_DATE = "startDate";
+    public static final String END_DATE = "endDate";
+
+    public CommitmentData fromJson(String jsonCommitment) {
+        Gson gson = new Gson();
+
+        JsonObject json = gson.fromJson(jsonCommitment, JsonObject.class);
+
+        return new CommitmentData(
+            new CraftspersonId(json.get(CRAFTSPERSON_ID).getAsString()),
+            new ProjectId(json.get(PROJECT_ID).getAsString()),
+            LocalDate.parse(json.get(START_DATE).getAsString()),
+            LocalDate.parse(json.get(END_DATE).getAsString())
+        );
+    }
+}
---
+++ src/test/java/com/codurance/merlin/infrastructure/CommitmentDataJsonTransformerShould.java
@@ -0,0 +1,49 @@
+package com.codurance.merlin.infrastructure;
+
+import com.codurance.merlin.commitment.CommitmentData;
+import com.codurance.merlin.commitment.CraftspersonId;
+import com.codurance.merlin.commitment.ProjectId;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommitmentDataJsonTransformerShould {
+    public static final String CRAFTSPERSON_ID = "craftsperson1";
+    public static final String PROJECT_ID = "project1";
+    public static final String START_DATE = "2017-10-10";
+    public static final String END_DATE = "2017-12-10";
+
+    private CommitmentDataJsonTransformer transformer;
+
+    @Before
+    public void setUp() {
+        transformer = new CommitmentDataJsonTransformer();
+    }
+
+    @Test
+    public void transformJsonToCommitmentData() throws Exception {
+        assertThat(transformer.fromJson(aCommitmentJson()))
+                .isEqualTo(aCommitmentData());
+    }
+
+    protected CommitmentData aCommitmentData() {
+        return new CommitmentData(
+                new CraftspersonId(CRAFTSPERSON_ID),
+                new ProjectId(PROJECT_ID),
+                LocalDate.parse(START_DATE),
+                LocalDate.parse(END_DATE)
+        );
+    }
+
+    private String aCommitmentJson() {
+        return "{" +
+                "\"craftspersonId\": \"" + CRAFTSPERSON_ID + "\"," +
+                "\"projectId\": \"" + PROJECT_ID + "\"," +
+                "\"startDate\": \"" + START_DATE + "\"," +
+                "\"endDate\": \"" + END_DATE + "\"" +
+                "}";
+    }
+}
